b8a08d1fad0ac0a6f5e8c827c91f58ffc5017e63
Vinod Kumar <vkvinodkumar71117@gmail.com>
2024-10-15 22:55:36 +0530

    Your commit message

--- hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>  // Include the input-output stream library
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;  // Output the text "Hello, World!"
-    return 0;  // Return 0 to indicate successful execution
-}
