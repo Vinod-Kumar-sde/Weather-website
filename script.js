bc796832144ec3df3f9aa233d934fa57a9370280
Vinod Kumar <vkvinodkumar71117@gmail.com>
2024-10-18 20:18:47 +0530

    thank you chatgpt

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
-const Container = document.querySelector('.card');
+const container = document.querySelector('.card');
 const search = document.querySelector('.search button');
 
 const weather = document.querySelector('.weather');
-const weatherData= document.querySelector('.details');
+const weatherData = document.querySelector('.details');
 
 const searchBox = document.querySelector('.search input');
-  const resultDiv = document.querySelector('.weather .city');
+const resultDiv = document.querySelector('.weather .city');
 
-  searchBox.addEventListener('input', function() {
+// Display input value (city) in the resultDiv
+searchBox.addEventListener('input', function() {
     const input = searchBox.value.toUpperCase();
     resultDiv.textContent = `${input}`;
-  });
+});
 
-search.addEventListener('click', () => {
+// Function to fetch and display weather data
+const fetchWeather = () => {
     const APIKey = 'b12cf716a8687424f749654e9e871469';
-    const city = document.querySelector('.search input').value;
-    if(city == '')
-        return;
+    const city = searchBox.value;
+    if (city == '') return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
-
-        const Image = document.querySelector('.weather img');
+    // Fetch weather data from the API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(json => {
+        // Get the necessary DOM elements
+        const image = document.querySelector('.weather img');
         const temperature = document.querySelector('.weather .temp');
-        const City = document.querySelector('.weather .city');
+        const cityElement = document.querySelector('.weather .city');
         const description = document.querySelector('.weather .description');
-        const humidity = document.querySelector('.weather .humidity');
-        const wind = document.querySelector('.weather .wind');
+        const humidityElement = document.querySelector('.weather .humidity');
+        const windElement = document.querySelector('.weather .wind');
 
-        switch(json.weather[0].main) {
+        // Set the weather icon based on conditions
+        switch (json.weather[0].main) {
             case 'Clear':
-                Image.src = 'images/clear.png';
+                image.src = 'images/clear.png';
                 break;
             case 'Clouds':
-                Image.src = 'images/clouds.png';
+                image.src = 'images/clouds.png';
                 break;
-            case 'Rainy':
-                Image.src = 'images/rain.png';
+            case 'Rain':
+                image.src = 'images/rain.png';
                 break;
-            case 'Humid':
-                Image.src = 'images/humidity.png';
+            case 'Humidity':
+                image.src = 'images/humidity.png';
                 break;
-            case 'Snowy':
-                Image.src = 'images/snow.png';
+            case 'Snow':
+                image.src = 'images/snow.png';
                 break;
             case 'Mist':
-                Image.src = 'images/mist.png';
-                break;
             case 'Haze':
-                Image.src = 'images/mist.png';
+                image.src = 'images/mist.png';
                 break;
             default:
                 image.src = 'images/clouds.png';
         }
 
+        // Display weather information
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°c</span>`;
         description.innerHTML = `${json.weather[0].description}`;
-        Humidity.innerHTML = `${json.main.humidity}%`;
-        wind.innerHTML = `${parseInt(json.wind.temp)}`;
-        
+        humidityElement.innerHTML = `${json.main.humidity}%`;
+        windElement.innerHTML = `${parseInt(json.wind.speed)} km/h`;
     });
+};
+
+// Trigger search on click
+search.addEventListener('click', fetchWeather);
+
+// Trigger search when pressing "Enter"
+searchBox.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        fetchWeather();
+    }
 });
