d555eebe46092d4dceb1c674a5ae6d53ec713fdb
Vinod Kumar <vkvinodkumar71117@gmail.com>
2024-10-18 17:02:43 +0530

    finally javascript half

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-const url = 'https://open-weather13.p.rapidapi.com/city/bengaluru/EN';
-const options = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': '016e00501amsha3f929f49d764d9p1887aejsn0505d5143d48',
-        'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-    }
-};
+const Container = document.querySelector('.card');
+const search = document.querySelector('.search button');
 
-// Wrapping it inside an async function
-async function fetchWeather() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
+const weather = document.querySelector('.weather');
+const weatherData= document.querySelector('.details');
 
-// Call the async function to execute it
-fetchWeather();
+const searchBox = document.querySelector('.search input');
+  const resultDiv = document.querySelector('.weather .city');
+
+  searchBox.addEventListener('input', function() {
+    const input = searchBox.value.toUpperCase();
+    resultDiv.textContent = `${input}`;
+  });
+
+search.addEventListener('click', () => {
+    const APIKey = 'b12cf716a8687424f749654e9e871469';
+    const city = document.querySelector('.search input').value;
+    if(city == '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+
+        const Image = document.querySelector('.weather img');
+        const temperature = document.querySelector('.weather .temp');
+        const City = document.querySelector('.weather .city');
+        const description = document.querySelector('.weather .description');
+        const humidity = document.querySelector('.weather .humidity');
+        const wind = document.querySelector('.weather .wind');
+
+        switch(json.weather[0].main) {
+            case 'Clear':
+                Image.src = 'images/clear.png';
+                break;
+            case 'Clouds':
+                Image.src = 'images/clouds.png';
+                break;
+            case 'Rainy':
+                Image.src = 'images/rain.png';
+                break;
+            case 'Humid':
+                Image.src = 'images/humidity.png';
+                break;
+            case 'Snowy':
+                Image.src = 'images/snow.png';
+                break;
+            case 'Mist':
+                Image.src = 'images/mist.png';
+                break;
+            case 'Haze':
+                Image.src = 'images/mist.png';
+                break;
+            default:
+                image.src = 'images/clouds.png';
+        }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°c</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        Humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.temp)}`;
+        
+    });
+});
